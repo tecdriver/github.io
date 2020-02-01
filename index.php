abbf60a1bc721d09c1f1d67f310050312d9c1162
tecdriver <60532539+tecdriver@users.noreply.github.com>
2020-01-31 22:09:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<script src="jquery-3.4.1.min.js"></script>
+		<script src="gama.js"></script>		
+	</head>
+	<body>
+		<div id="uno">
+			BIENVENIDO1
+		</div>
+		<div id="dos">
+			BIENVENIDO2
+		</div>
+		<input type="button" id="mostrar" value="mostrar">
+		<input type="button" id="esconder" value="esconder">
+	</body>
+</html>
+
+
+
+
+
+
+
